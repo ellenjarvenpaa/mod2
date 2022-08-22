eabc4111e7df366be3633d24ec13a5d1854fa6d4
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2022-08-22 14:43:41 +0300

    eka commit

---
+++ teht.2.3.py
@@ -0,0 +1,8 @@
+kanta = float(input("anna suorakulmion kanta"))
+korkeus = float(input("anna suorakulmion korkeus"))
+
+ala = (kanta * korkeus)
+piiri = (kanta * 2 +korkeus * 2)
+
+print("suorakulmion ala on" + str(ala))
+print("suorakulmion piiri on" + str(piiri))
