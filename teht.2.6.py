d65f609e0a49c234aa68681e6ef831bb08ccef25
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2022-08-22 19:07:46 +0300

    eka commit

--- teht.2.6.py
+++ teht.2.6.py
@@ -1,4 +1,12 @@
 import random
 
 x = random.randint(0, 9)
-print(x)
+y = random.randint(0, 9)
+z = random.randint(0, 9)
+print(x, y, z)
+
+a = random.randint(1, 6)
+b = random.randint(1, 6)
+c = random.randint(1, 6)
+d = random.randint(1, 6)
+print(a, b, c, d)
