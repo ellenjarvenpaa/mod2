0b55d959a2c8880d98104efe74b7cd0b98f727f4
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2022-08-22 14:52:33 +0300

    eka commit

---
+++ teht.2.4.py
@@ -0,0 +1,11 @@
+luku_1_ = int(input("anna 1. kokonaisluku"))
+luku_2_ = int(input("anna 2. kokonaisluku"))
+luku_3_ = int(input("anna 3. kokonaisluku"))
+
+summa = luku_1_ + luku_2_ + luku_3_
+tulo = luku_1_ * luku_2_ * luku_3_
+keskiarvo = summa / 3
+
+print(summa)
+print(tulo)
+print(keskiarvo)
